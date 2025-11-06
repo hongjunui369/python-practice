39d9fdea2611d65b54a79cbdf3ca995e9ac7770b
HongJunui <hongjunui369@gmail.com>
2025-11-06 17:38:19 +0900

    using TK

---
+++ 숫자추측.py
@@ -0,0 +1,45 @@
+from tkinter import *
+import random
+
+answer = random.randint(1,100)
+
+def guessing():
+	guess = int(guessField.get()) # 텍스트 필드에서 사용자가 입력한 값을
+				# 가져온다.
+
+	if guess > answer:
+		msg = "그거보다 낮은 숫자야"
+	elif guess < answer:
+		msg = "그거보다 높은 숫자야"
+	else:
+		msg = "정답! 축하한다이"
+
+	resultLabel["text"] = msg # 메시지를 출력한다.
+	guessField.delete(0, 5)
+def reset(): # 정답 초기화
+	global answer
+	answer = random.randint(1,100)
+	resultLabel["text"] = "다시 한번 하세요!"
+
+window = Tk()
+window.configure(bg="white")
+window.title("숫자를 맞춰보세요!")
+
+window.geometry("500x200")
+titleLabel = Label(window, text="숫자 게임에 오신 것을 환영합니다!",
+bg="white")
+titleLabel.pack()
+guessField = Entry(window)
+guessField.pack(side="left")
+tryButton = Button(window, text="시도", fg="green", bg="white",
+command=guessing )
+tryButton.pack(side="left")
+
+resetButton = Button(window, text="초기화", fg="red", bg="white",
+command=reset)
+resetButton.pack(side="left")
+resultLabel = Label(window, text="1부터 100사이의 숫자를 입력하시오.",
+bg="white")
+resultLabel.pack(side="left")
+
+window.mainloop()
